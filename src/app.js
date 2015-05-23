f16d3b5163e7bdb22afc557058d45d3daa8c94a5
Jichao Ouyang <oyanglulu@gmail.com>
2015-05-23 10:23:41 +0800

    webpack scss

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 let riot = require('riot');
+require('primer-css/scss/primer.scss')
+require('foundation/scss/foundation/components/_grid.scss')
 require('./pages/todo.html');
 require('./pages/login.html');
 require('./router.sjs');
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 var webpack = require('webpack');
-
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require('path');
+const sassLoaders = [
+  "css-loader",
+  "autoprefixer-loader?browsers=last 2 version",
+  "sass-loader?includePaths[]=" + path.resolve(__dirname, "./scss"),
+];
 module.exports = {
   cache: true,
   entry: {
@@ -8,22 +14,29 @@ module.exports = {
   output: {
     path: './javascripts/',
     publicPath: '/javascripts/',
-    filename: 'app.js'
+    filename: '[name].js'
   },
   module: {
     preLoaders: [
       { test: /\.html$/, include: /src|tests/, loader: 'riotjs' },
     ],
     loaders: [
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract("style-loader", sassLoaders.join("!")),
+      },
       { test: /\.sjs$/, loader: 'sweetjs?modules[]=../node_modules/sparkler/macros/index.js'},
-      { test: /\.css$/, include: /src|tests/, loader: 'style!css' },
-      { test: /\.js$|\.html$/, include: /src|tests/, loader: 'babel?stage=1&optional=runtime&blacklist=strict'}
+      { test: /\.js$|\.html$/, include: /src|tests/, loader: 'babel?stage=1&optional=runtime&blacklist=strict'},
     ]
+  },
+  resolve: {
+    modulesDirectories: ["src", "node_modules", "bower_components", "scss"],
   },
   plugins: [
     new webpack.ProvidePlugin({
       riot: 'riot'
-    })
+    }),
+    new ExtractTextPlugin('../stylesheets/[name].css')
   ],
   devServer: {
     port: 5555
